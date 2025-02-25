a508a419fdf549e26d5be27f0ff0ddfe68b468ea
Magamed56 <94898458+Magamed56@users.noreply.github.com>
2025-02-26 00:32:45 +0600

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -73,7 +73,8 @@ USE_TZ = True
 
 # Статические файлы
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Медиафайлы
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
